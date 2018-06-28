0ccd9df49874bf8749d0080c5e2f61a518ecaf08
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-06-28 15:11:32 -0700

    Complete

--- 234-palindrome-linked-list.js
+++ 234-palindrome-linked-list.js
@@ -10,3 +10,38 @@
 //
 // Follow up:
   // Could you do it in O(n) time and O(1) space?
+
+var isPalindrome = function(head) {
+  if (!head) return true;
+
+  let tortoise = head;
+  let hare = head;
+  let count = 1;
+
+  let prev = null;
+  while (hare && hare.next) {
+    hare = hare.next.next;
+    count += 1;
+    if (hare) {
+      count += 1;
+    }
+
+    const next = tortoise.next;
+    tortoise.next = prev;
+    prev = tortoise;
+    tortoise = next;
+  }
+
+  let left = prev;
+  let right = tortoise;
+  if (count % 2 != 0) {
+    right = tortoise.next;
+  }
+
+  while (left && right && left.val === right.val) {
+    left = left.next;
+    right = right.next;
+  }
+
+  return (left === null && right === null);
+};
