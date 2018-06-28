b4a60795c642d4f95542b3f1f3d7fc7ca1df75f8
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-06-28 16:00:43 -0700

    Complete

--- 108-convert-sorted-array-to-binary-search-tree.js
+++ 108-convert-sorted-array-to-binary-search-tree.js
@@ -8,10 +8,33 @@
 // Example:
 // Given the sorted array: [-10,-3,0,5,9],
 //
-// One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+// One possible answer is: [0,-3,9,-10,null,5],
+// which represents the following height balanced BST:
 //
 //       0
 //      / \
 //    -3   9
 //    /   /
 //  -10  5
+
+// Definition for a binary tree node.
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+
+// @param {number[]} nums
+// @return {TreeNode}
+
+
+var sortedArrayToBST = function(nums) {
+  if (nums.length === 0) return null;
+
+  const mid = Math.floor(nums.length / 2);
+  const node = new TreeNode(nums[mid]);
+
+  node.left = sortedArrayToBST(nums.slice(0, mid));
+  node.right = sortedArrayToBST(nums.slice(mid+1));
+
+  return node;
+};
