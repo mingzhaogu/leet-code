58cf6e649c1824e7a14a58071901fed26fdd6336
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-07-17 21:06:02 -0700

    Start

---
+++ connected-robots.js
@@ -0,0 +1,15 @@
+// You are given a list of robots, e.g.
+// [a, b, c, d, e, f, g]
+//
+// And a list of connections, which is an array of tuples
+// conn1 = [[a, b], [b, c], [c, d], [e, f], [f, g]]
+//
+// You are tasked to find the groups of robots that are connected to each
+// other.
+//
+// In the example above,
+// a-b-c-d
+// e-f-g
+//
+// There are two groups of robots, the expected return is
+// [[a, b, c, d], [e, f, g]]
